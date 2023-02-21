757744f3a72c6c8935d294a480a477e5fd6e7151
Sai <sai.subramaniam@iopex.com>
2023-02-21 15:29:52 +0530

    Variable file

---
+++ variable.py
@@ -0,0 +1,18 @@
+#type 1
+a = 10
+b = 20
+c = "welcome"
+print(a)
+print(b)
+print(c)
+
+#type 2
+a,b,c = "Sai", 3339, "Test Engineer"
+print(a)
+print(b)
+print(c)
+
+#type 3
+a,b,c = "Sai", 3339, "Test Engineer"
+print(a, b, c)
+
